2f1793b022c7e0ba0aecab600f8926b538dc5651
Mariano Telesca <mariano.a.telesca@gmail.com>
2021-12-01 18:28:07 -0300

    Agregado carrito. Arreglado botones de cada cerveza

--- js/formulario.js
+++ js/formulario.js
@@ -1,28 +1,38 @@
+
+// Función que valida que los datos necesarios sean completados en el Formulario de Contacto
 function validarForm(){
-    if($("#nombre").val() == ""){
+    if($("#form__nombre").val() == ""){
         alert("El nombre no puede estar vacío.");
-        $("#nombre").focus();
+        $("#form__nombre").focus();
         return false;
     }
-    if($("#apellido").val() == ""){
+    if($("#form__email").val() == ""){
+        alert("Debe colocar una dirección de email");
+        $("#form__email").focus();
+        return false;
+    }
+    if($("#form__apellido").val() == ""){
         alert("El campo apellido no puede estar vacío.");
-        $("#apellido").focus();
+        $("#form__apellido").focus();
         return false;
     }
-    if($("#consulta").val() == ""){
-        alert("No puede dejar la consulta en blanco");
-        $("#consulta").focus();
-        return false;
-    }
-    if(!$("#mayorEdad").is(":checked")){
+    if(!$("form__#mayorEdad").is(":checked")){
         alert("Debe confirmar que es mayor de 18 años.");
+        return false;
+    }
+    if($("#form__consulta").val() == ""){
+        alert("No puede dejar la consulta en blanco");
+        $("#form__consulta").focus();
         return false;
     }
 
     return true;
 }
 
+
 $(document).ready( function() {
+
+    // Confirmación de envio del formulario en caso de validación exitosa
     $("#botonEnviarForm").click( function() { 
         if(validarForm()){                      
            $("#formularioContacto").slideUp("slow");   
--- js/main.js
+++ js/main.js
@@ -11,91 +11,60 @@ class Cerveza {
     }
 }
 
-// class Cliente {
-//     constructor(id, nombre) {
-//         this.id = id;
-//         this.nombre = nombre;
-//     }
-// }
-
-// class Pedido {
-
-//     constructor(id, cliente, cerveza){
-//         this.id = id;
-//         this.cliente = cliente;
-//         this.cerveza = cerveza;
-//     }
-
-//     mostrarHTML(){
-//         return `<h1>${this.id}</h1><p> ${this.cliente.nombre}, compraste la cerveza: ${this.cerveza.marca} por ${this.cerveza.precio}</p>`;
-//     }
-// }
-
 let listaCerveza = [];
-// let listaPedidos = [];
-
 let cerveza1 = new Cerveza(1, "Patagonia", "Bohemian Pilsner", 110, "../imagenes/cerveza_patagonia.jpg", "Es una cerveza que se caracteriza por su color dorado brillante y aroma maltoso, como a pan. IBU 18, 5.1% graduación alcoholica.");
 let cerveza2 = new Cerveza(2, "Andes", "Roja", 100, "../imagenes/cervezas_andes.jpg", "Es una cerveza estilo Vienna Lager, su aroma a suave caramelo y leve lúpulo logran un balance perfecto. De color ámbar cobrizo. IBU 18, 5,1% graduación alcoholica.");
 let cerveza3 = new Cerveza(3, "Temple", "Honey", 120, "../imagenes/cerveza_temple.jpg", "Cerveza roja. Tiene un perfil a tostado y caramelo. De color cobrizo. 5.5% de graduación alcoholica y 24 de ibu (Amargor).");
 let cerveza4 = new Cerveza(4, "Pampa Brewing", "Amber Ale", 150, "../imagenes/cerveza_pampa.jpg", "Sabor moderado a malta caramelo y toffee. IBU 20, 4.9% graduación alcoholica.");
 let cerveza5 = new Cerveza(5, "Peñon del Aguila", "Kolsch", 160, "../imagenes/cerveza_pda.jpg", "Estilo alemán, de la zona de Colonia. De color dorado pálido y cuerpo liviano. Presenta aromas a lúpulo y cítricos.");
-
 listaCerveza.push(cerveza1, cerveza2, cerveza3, cerveza4, cerveza5);
 
+
+
+
 $(document).ready(()=>{
-    for(Cerveza of listaCerveza){
+    for(i = 0; i<listaCerveza.length; i++){
+        let objetosCerveza = listaCerveza[i];
+        // Creación de las Cards con cada una de los productos instanciados
        $("#listaCervezas").append(` 
        <div class="col" >
           <div class="card">
-            <img src="${Cerveza.imagen}" class="card-img-top" alt="Latas de cerveza marca ${Cerveza.marca} en todas las variedades disponibles">
+            <img src="${objetosCerveza.imagen}" class="card-img-top" alt="Latas de cerveza marca ${objetosCerveza.marca} en todas las variedades disponibles">
             <div class="card-body">
-                <h2 class="card-title"> ${Cerveza.marca} </h2>
-                <p class="card-text"> ${Cerveza.sabor} : ${Cerveza.descripcion} </p> 
-                <p> <button id="btnCerveza${Cerveza.id}"> Agregar al carrito </button> </p> 
+                <h2 class="card-title"> ${objetosCerveza.marca} </h2>
+                <p class="card-text"> ${objetosCerveza.sabor} : ${objetosCerveza.descripcion} </p> 
+                <p class="card-text"> ${objetosCerveza.precio} </p>
+                <p> <button id="btnCerveza${objetosCerveza.id}"> Agregar al carrito </button> </p> 
             </div> 
           </div> 
         </div>`)
    
-        $(`#btnCerveza${Cerveza.id}`).click(()=>{
-            console.log(`BIEN ${Cerveza.precio}`);
-            alert(`Elegiste la ${Cerveza.marca}, a $ ${Cerveza.precio}`);
+        // Evento de click de los botones creados para el "registro" del producto elegido dentro del carrito de compras
+        $(`#btnCerveza${objetosCerveza.id}`).click(()=>{
+            objetosCerveza.cantidad = objetosCerveza.cantidad + 1
+            $(`.carritoCompras`).append(`   <img src="${objetosCerveza.imagen}" class="imgCarrito" alt="Latas de cerveza marca ${objetosCerveza.marca}">
+                                            <div class="">
+                                                <p class=""> ${objetosCerveza.marca} </p>
+                                                <p class=""> ${objetosCerveza.sabor} </p> 
+                                                <p> $ ${objetosCerveza.precio} x ${objetosCerveza.cantidad} </p> 
+                                            </div> `);
+            $(`.imgCarrito`).css( { "width":"10vw",
+                                    "height":"10vh" } );
+            console.log(`${objetosCerveza.cantidad}`);
+        });
+
+
+
+        //Acción de que aparezca y desaparezca el carrito a traves de un boton en la barra de navegacion
+        $(`.carritoComprasShow`).click(()=>{
+            $(`.carritoCompras`).css( { "position":"relative",
+                                        "overflow":"scroll",
+                                        "z-index":"10",
+                                        "border" : "solid 1 black",
+                                        } );
+
+            $(`.carritoCompras`).fadeToggle("slow");
         });
    
     };
 })
-
-
-// let contadorPedido = 1;
-// let nombre = $("#nombre").val();
-// let cliente = new Cliente(1, nombre);
-
-// function validarForm(){
-//     if($("#nombre").val() == ""){
-//         alert("El campo Nombre no puede estar vacío.");
-//         $("#nombre").focus();
-//         return false;
-//     }
-//     if($("#eleccionCerveza").val() == ""){
-//         alert("No puede dejar vacia la eleccion de la cerveza");
-//         $("#eleccionCerveza").focus();
-//         return false;
-//     }
-
-//     return true;
-// }
-
-// $(document).ready( function() {
-//     $("#botonenviar").click( function() { 
-//         if(validarForm()){                      
-//            $("#formulario").fadeOut("slow");   
-//            $("#exito").fadeIn("slow");
-//            $("#descripcionPedido").append(`${pedido.mostrarHTML()}`)                     
-//         }
-//     });    
-// });
-
-// let numeroCervezaElegida = $("#eleccionCerveza").val();
-// let cervezaElegida = listaCerveza.find( cerveza => cerveza.id === numeroCervezaElegida );                                
-// let pedido = new Pedido(contadorPedido, cliente, cervezaElegida);
-// listaPedidos.push(pedido);
-// localStorage.setItem("pedido", pedido);
